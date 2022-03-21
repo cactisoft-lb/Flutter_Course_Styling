5f2cf075da04c596ff3f4150f6e7c025e342086b
cactisoft-lb <cactisoft.lb@gmail.com>
2022-03-21 11:26:50 +0200

    more conatiner , text widgets Styling

--- lib/main.dart
+++ lib/main.dart
@@ -47,14 +47,45 @@ class MyHomePage extends StatelessWidget {
             children: transactions.map((transaction) {
               return Card(
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
+                    // Styling Container => adding some styling properties
                     Container(
-                      child: Text(transaction.amount.toString()),
+                      child: Text(
+                        transaction.amount.toString(),
+                        //text Styling properties
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                      padding: EdgeInsets.all(11),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 2, color: Colors.purple),
+                      ),
                     ),
+                    //styling the body of the Card
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(transaction.title),
-                        Text(transaction.date.toString()),
+                        Text(
+                          transaction.title,
+                          style: TextStyle(
+                              color: Colors.purple[400],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        Text(
+                          transaction.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                            fontSize: 15,
+                          ),
+                        ),
                       ],
                     ),
                   ],
