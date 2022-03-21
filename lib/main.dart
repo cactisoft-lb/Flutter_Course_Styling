0a6d713f207942ea479978eee5e0d9afad768291
cactisoft-lb <cactisoft.lb@gmail.com>
2022-03-21 09:24:40 +0200

    creating Transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './models/transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,6 +15,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'new Clothes', amount: 62.23, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Shoes', amount: 112.70, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
