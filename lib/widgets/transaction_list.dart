2e055758ad9b9a4e2f40b285cecdc2b85fdd6fe6
cactisoft-lb <cactisoft.lb@gmail.com>
2022-03-24 16:22:55 +0200

    wrapping our columns with SingleChildScrollView to get srolling fuctionality

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,55 +9,60 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Card(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              // Styling Container => adding some styling properties
-              Container(
-                child: Text(
-                  "\$${transaction.amount}",
-                  //text Styling properties
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple,
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
-                padding: EdgeInsets.all(11),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.purple),
-                ),
-              ),
-              //styling the body of the Card
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 500,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((transaction) {
+            return Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    transaction.title,
-                    style: TextStyle(
-                        color: Colors.purple[400],
+                  // Styling Container => adding some styling properties
+                  Container(
+                    child: Text(
+                      "\$${transaction.amount}",
+                      //text Styling properties
+                      style: TextStyle(
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  Text(
-                    //using itl DateFormat
-                    DateFormat.yMMMd().format(transaction.date),
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 15,
+                        color: Colors.purple,
+                      ),
                     ),
+                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                    padding: EdgeInsets.all(11),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.purple),
+                    ),
+                  ),
+                  //styling the body of the Card
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                            color: Colors.purple[400],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      Text(
+                        //using itl DateFormat
+                        DateFormat.yMMMd().format(transaction.date),
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
